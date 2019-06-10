f5aa88883820b89986bd9557cc2852dec10a01f3
李明 <1030839531@qq.com>
2019-06-10 22:45:28 +0800

    add gitignore and fix bug

--- Chapter1/practice/1.1.sql
+++ Chapter1/practice/1.1.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Jyushoroku (
   toroku_bango INTEGER NOT NULL COMMENT '注册编号',
-  NAME VARCHA`jyushoroku`R(128) NOT NULL COMMENT '姓名',
+  NAME VARCHAR(128) NOT NULL COMMENT '姓名',
   jyusho VARCHAR(256) NOT NULL COMMENT '住址',
   tel_no CHAR(10) COMMENT '电话号码',
   mail_address CHAR(20) COMMENT '邮件地址',
