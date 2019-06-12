8d02082a182b03b5ea4714a9381d54944d16887b
李明 <1030839531@qq.com>
2019-06-12 22:55:32 +0800

    优化

--- Chapter1/example/CreateTableShohin.sql
+++ Chapter1/example/CreateTableShohin.sql
@@ -1,9 +1,9 @@
 CREATE TABLE shohin (
-  shohin_id CHAR(4) NOT NULL,
-  shohin_mei VARCHAR(100) NOT NULL,
-  shohin_bunrui VARCHAR(32) NOT NULL,
-  hanbai_tanka INTEGER,
-  shiire_tanka INTEGER,
-  torokubi DATE,
+  shohin_id CHAR(4) NOT NULL COMMENT '商品编号',
+  shohin_mei VARCHAR(100) NOT NULL COMMENT '商品名称',
+  shohin_bunrui VARCHAR(32) NOT NULL COMMENT '商品分类',
+  hanbai_tanka INTEGER COMMENT '销售单价',
+  shiire_tanka INTEGER COMMENT '进货单价',
+  torokubi DATE COMMENT '登记日期',
   PRIMARY KEY (shohid_id)
 );
